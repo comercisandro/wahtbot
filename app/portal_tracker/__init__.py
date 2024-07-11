efcc47112558990ec5c5fcade91bf295250ce957
sandro.comerci <sandro.comerci@accenture.com>
2024-07-11 13:30:08 -0300

    portales

--- app/portal_tracker/__init__.py
+++ app/portal_tracker/__init__.py
@@ -1,4 +1,4 @@
-from app.portal_tracker.views import tracker_endpoint
+from app.portal_tracker.views import tracker_endpoint, inventario_endpoint
 
 
-__all__ = ["tracker_endpoint"]
+__all__ = ["tracker_endpoint", "inventario_endpoint"]
--- app/portal_tracker/views.py
+++ app/portal_tracker/views.py
@@ -2,9 +2,21 @@ from flask import jsonify, request, Blueprint, render_template
 import pandas as pd
 
 tracker_endpoint = Blueprint("tracker", __name__)
+inventario_endpoint = Blueprint("inventario", __name__)
 
-@tracker_endpoint.route("/tracker", methods=['GET'])
+@tracker_endpoint.route("/", methods=['GET'])
 def handler_tracker():
+    return render_template('portal.html')
 
-    return "futuro portal"
 
+# Ruta para mostrar el inventario
+@inventario_endpoint.route("/inventario", methods=['GET'])
+def inventario():
+    df = pd.read_csv("data/inventario.csv", delimiter=';')
+    data = df.to_dict(orient='records')
+    return render_template('inventario.html', data=data)
+
+# Ruta para descargar el inventario como archivo Excel
+# @app.route("/download", methods=['GET'])
+# def download():
+#     return send_file("inventario.xlsx", as_attachment=True)
